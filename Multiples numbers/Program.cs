c04ac7b9af89258e9c10b405dbae21cb283a5b14
AleksandrGHub <bulanov.shura@gmail.com>
2024-06-05 21:14:07 +0300

    Добавьте файлы проекта.

---
+++ Multiples numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Multiples_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 100;
+            int maxNumber = 999;
+            int minRandomNumber = 1;
+            int maxRandomNumber = 28;
+            int quantityNaturalNumber = 0;
+            int reduced;
+            int randomNumber;
+            Random random = new Random();
+
+            randomNumber = random.Next(minRandomNumber, maxRandomNumber);
+
+            for (int i = minNumber; i <= maxNumber; i++)
+            {
+                for (reduced = i; reduced >= randomNumber; reduced -= randomNumber)
+                {
+                    if (reduced == randomNumber)
+                    {
+                        quantityNaturalNumber++;
+                    }
+                }
+            }
+
+            Console.WriteLine(quantityNaturalNumber);
+        }
+    }
+}
